90f0e7256b1a4d57a29e86adcca83d4701531b63
Pekka Helenius <fincer89@hotmail.com>
2019-06-08 14:37:27 +0300

    Update year stamp

--- ssmtp_conf-sample/wanchecker.sh
+++ ssmtp_conf-sample/wanchecker.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 #    WAN IP Checker - Whenever server WAN IP address changes, inform admins via email
-#    Copyright (C) 2018  Pekka Helenius
+#    Copyright (C) 2019  Pekka Helenius
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
